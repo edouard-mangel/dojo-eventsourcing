010c0b75074738cd6e74e4e046354991ed3330c4
Edouard <mangel.edouard.pro@gmail.com>
2025-04-14 14:15:27 +0200

    New ampoule

---
+++ thinkbeforecoding/Ampoule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thinkbeforecoding;
+internal class Ampoule
+{
+    public string Id { get; set; }
+    public int NbUtilisationsRestantes { get; private set; } = 10;
+
+    public Ampoule(string Id, int nbUtilisationsRestantes, bool estAllume )
+    {
+        this.Id = Id;
+        NbUtilisationsRestantes = nbUtilisationsRestantes;
+        EstAllume = estAllume;
+    }
+
+    public bool EstAllume { get; set; } = false;
+    public Ampoule Allumer()
+    {
+        if (!EstAllume && NbUtilisationsRestantes > 0){
+            return new Ampoule(this.Id, NbUtilisationsRestantes -1 , true);
+        }
+        return this; 
+    }
+
+    public Ampoule Eteindre()
+    {
+        if (EstAllume)
+        {
+            return new Ampoule(this.Id, this.NbUtilisationsRestantes, false);
+        }
+        return this; 
+    }
+
+    public void SauvegarderSurDisque()
+    {
+        // Ecrire l'état de l'ampoule dans un fichier 
+        string filePath = "ampoule.txt";
+
+        // Création d'une instance de StreamWriter
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            // Écriture de texte dans le fichier
+            writer.WriteLine(this.NbUtilisationsRestantes.ToString() + ' ' + this.EstAllume.ToString());
+        }
+
+        Console.WriteLine("Écriture dans le fichier terminée.");
+    }
+
+
+}
---
+++ thinkbeforecoding/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace thinkbeforecoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ampoule, allumer.");
+            var ampoule = getAmpoule("ampoule");
+
+            ampoule = ampoule.Eteindre();
+            ampoule = ampoule.Allumer();
+            ampoule = ampoule.Eteindre();
+            ampoule = ampoule.Allumer();
+            ampoule = ampoule.Eteindre();
+            ampoule.SauvegarderSurDisque();
+        }
+
+        private static Ampoule getAmpoule(string id)
+        {
+            string filePath =  id + ".txt";
+
+            // Création d'une instance de StreamWriter
+            using (StreamReader reader = new(filePath))
+            {
+                // Écriture de texte dans le fichier
+                var data = reader.ReadLine().Split(' ');
+                return new Ampoule(id, int.Parse(data[0]),bool.Parse( data[1]));
+            }
+        }
+    }
+}
